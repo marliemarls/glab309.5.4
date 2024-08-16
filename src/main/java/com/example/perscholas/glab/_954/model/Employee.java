a1edffe98a3c2fc41f68616e632bd258e1d9a74d
Marlie Martinez <marlieahue@gmail.com>
2024-08-16 13:54:02 -0400

    first commit

---
+++ src/main/java/com/example/perscholas/glab/_954/model/Employee.java
@@ -0,0 +1,47 @@
+package com.example.perscholas.glab._954.model;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Employee {
+    private long id;
+    private String name;
+    private String contactNumber;
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", contactNumber='" + contactNumber + '\'' +
+                '}';
+    }
+}
